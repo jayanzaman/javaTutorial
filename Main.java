2030d003a451e31e6feca0dc20f37e00d150b9d3
jayanzaman <jayanzaman@gmail.com>
2017-04-12 15:07:05 -0500

    first java file from the Java intro class

--- Main.java
+++ Main.java
@@ -1,8 +1,22 @@
 package com.zaman;
 
+import java.awt.*;
+
+import static java.lang.System.*;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	out.println("Best IDE");
+	Frame win = new Frame();
+            out.println(win);
+    int[] intArray;
+    intArray = new int[2];
+    intArray[0] = 12;
+    intArray[1] = 6;
+    int i;
+    for( i = 0; i < intArray.length; i++){
+        out.println(intArray[i]);
+    }
     }
 }
